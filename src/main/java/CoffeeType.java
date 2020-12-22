baf0e9f02045ec4a6c4c89d0abf698923e6f7c74
Derek Somerville <derek.somerville@glasgow.ac.uk>
2020-12-22 16:12:00 +0000

    Make A Factory

--- src/main/java/CoffeeType.java
+++ src/main/java/CoffeeType.java
@@ -3,4 +3,5 @@ public class CoffeeType {
     public static final String cappuccino = "Cappuccino";
     public static final String espresso = "Espresso";
     public static final String flatWhite = "Flat White";
+    public static final String mocha = "Mocha";
 }
--- src/main/java/OrderCoffee.java
+++ src/main/java/OrderCoffee.java
@@ -2,18 +2,8 @@ public class OrderCoffee {
 
     public Coffee orderCoffee(String typeOfCoffee){
         Coffee coffee;
-
-        if (typeOfCoffee == CoffeeType.americano){
-            coffee = new Americano();
-        } else if (typeOfCoffee == CoffeeType.cappuccino){
-            coffee = new Cappuccino();
-        } else if (typeOfCoffee == CoffeeType.espresso){
-            coffee = new Espresso();
-        } else if (typeOfCoffee == CoffeeType.flatWhite){
-            coffee = new FlatWhite();
-        } else {
-            coffee = new Americano();
-        }
+        CoffeeFactory coffeeFactory = new CoffeeFactory();
+        coffee = coffeeFactory.createCoffee(typeOfCoffee);
 
         coffee.prepare();
         coffee.getCoffeeCup();
@@ -25,7 +15,7 @@ public class OrderCoffee {
 
     public static void main(String[] args){
         OrderCoffee orderCoffee = new OrderCoffee();
-        Coffee coffee = orderCoffee.orderCoffee(CoffeeType.flatWhite);
+        Coffee coffee = orderCoffee.orderCoffee(CoffeeType.mocha);
         System.out.println(coffee.getName());
     }
 
